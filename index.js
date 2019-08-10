10ce487489e0f81f77a53ac9a2e3bfcd7a88f93d
Gary Daigle <zetaphor@gmail.com>
2019-08-09 20:28:03 -0400

    Remove Vue

--- index.js
+++ index.js
@@ -1,69 +1,84 @@
 window.onload = function () {
-  var app = new Vue({
-    el: '#app',
+  console.log('Loaded')
 
-    mounted () {
-      this.connectSteamVR()
+  window.app = {
+    wsUri: 'ws://127.0.0.1:8998/',
+    wsData: null,
+    controllerLeft: {},
+    controllerRight: {},
+    connectionStatus: 'Connecting...',
+    messages: [],
+    messageLimit: 10,
+    output: document.querySelector('#output'),
+
+    connect: function () {
+      app.addMessage('Open socket')
+      app.wsData = new WebSocket(app.wsUri)
+      app.wsData.onopen = app.onOpen
+      app.wsData.onclose = app.onClose
+      app.wsData.onerror = app.onError
+      app.wsData.onmessage = app.onMessage
     },
 
-    data: function () {
-      return {
-        wsUri: 'ws://127.0.0.1:8998/',
-        wsData: null,
-        controllers: { left: {}, right: {} }
+    onOpen: function (evt) {
+      app.wsData.send('mailbox_open controller_visualizer')
+      app.wsData.send('mailbox_send input_server {"type":"request_input_state_updates","device_path":"/user/hand/left","returnAddress":"controller_visualizer"}')
+      app.wsData.send('mailbox_send input_server {"type":"request_input_state_updates","device_path":"/user/hand/right","returnAddress":"controller_visualizer"}')
+    },
+
+    onClose: function () {
+      app.connectionStatus = 'Disconnected'
+      app.addMessage('Disconnected')
+    },
+
+    onError: function (evt) {
+      console.log('Error:', evt)
+      app.addMessage('Failed to connect')
+    },
+
+    onMessage: function (evt) {
+      let msgData = JSON.parse(evt.data)
+
+      if (typeof msgData.components === 'undefined') return
+
+      if (app.connectionStatus === 'Connecting...') {
+        app.connectionStatus = 'Connected To SteamVR'
+        app.addMessage('Connected')
       }
-    },
 
-    watch: {
-      controllers: function (newVal, oldVal) {
-        console.log('Update')
-      }
-    },
-
-    methods: {
-      connectSteamVR: function () {
-        console.log('Open data socket')
-        this.wsData = new WebSocket(this.wsUri)
-        this.wsData.onopen = this.onOpen
-        this.wsData.onclose = this.onClose
-        this.wsData.onerror = this.onError
-        this.wsData.onmessage = this.onMessage
-      },
-
-      onOpen: function (evt) {
-        console.log("Connected")
-        this.wsData.send('mailbox_open controller_visualizer')
-        this.wsData.send('mailbox_send input_server {"type":"request_input_state_updates","device_path":"/user/hand/left","returnAddress":"controller_visualizer"}')
-        this.wsData.send('mailbox_send input_server {"type":"request_input_state_updates","device_path":"/user/hand/right","returnAddress":"controller_visualizer"}')
-      },
-
-      onClose: function () {
-        console.log("Disconnected")
-      },
-
-      onError: function (evt) {
-        console.log('Error:', evt.data)
-      },
-
-      onMessage: function (evt) {
-        let msgData = JSON.parse(evt.data)
-        if (typeof msgData.components === 'undefined') return
-
-        for (const componentData in msgData.components) {
-          if (!msgData.components.hasOwnProperty(componentData)) continue
-          const componentValue = msgData.components[componentData]
-          let componentName = componentData.split('/')[2]
-          let valueType = componentData.split('/')[3]
-          console.log(componentData.split('/'))
-          let hand = null
-          if (msgData.device === '/user/hand/left') hand = 'left'
-          else if (msgData.device === '/user/hand/right') hand = 'right'
-
-          if (typeof this.controllers[hand][componentName] === 'undefined') this.controllers[hand][componentName] = {}
-          if (typeof this.controllers[hand][componentName][valueType] === 'undefined') this.controllers[hand][componentName][valueType] = 0
-          this.controllers[hand][componentName][valueType] = componentValue
+      for (const componentData in msgData.components) {
+        if (!msgData.components.hasOwnProperty(componentData)) continue
+        const componentValue = msgData.components[componentData]
+        let componentName = componentData.split('/')[2]
+        let valueType = componentData.split('/')[3]
+        let controller = {
+          hand: 'left',
+          data: null
         }
+
+        if (msgData.device === '/user/hand/left') controller.data = app.controllerLeft
+        else if (msgData.device === '/user/hand/right') controller = { hand: 'right', data: app.controllerRight }
+
+        if (controller && typeof controller.data[componentName] === 'undefined') {
+          app.addMessage(`${controller.hand} ${componentName} ${valueType} detected`)
+          controller.data[componentName] = {}
+        }
+        if (controller && typeof controller.data[componentName][valueType] === 'undefined') controller.data[componentName][valueType] = 0
+        controller.data[componentName][valueType] = componentValue
+
+        if (controller && controller.hand === 'left') app.controllerLeft = controller.data
+        else if (controller.hand === 'right') app.controllerRight = controller.data
       }
+      app.messages.push(controllers)
+      if (app.messages.length > app.messageLimit) app.messages.shift()
+    },
+
+    addMessage: function (message) {
+      app.output.innerHTML += `${message}<br />`
+      console.log('')
     }
-  })
+  }
+
+  app.connect()
 }
+
